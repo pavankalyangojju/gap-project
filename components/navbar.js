2c5cc2c5055af3468d3f5cf5c5e34575ff44061a
Tejaswini Prabhakar S <tejaswinishreeramwar105@gmail.com>
2022-01-23 13:13:52 +0530

    final gap project

---
+++ components/navbar.js
@@ -0,0 +1,68 @@
+function navbar(){
+
+    return  ` <div id="nav">
+    <div>
+      <img
+        width="100%"
+        height="80%"
+        src="https://brandwiki.ru/up/brands/product/190207_4635.jpg"
+      />
+    </div>
+    <div>
+      <a href="">OLD NAVY</a>
+      <a href="">BANANA REPUBLIC</a>
+      <a href="">ATHLETA</a>
+      <button>MY GAP</button>
+    </div>
+    <div>
+      <span>FREE SHIPPING ON $50+ FOR REWARDS MEMBERS</span>
+      <span><a href="">SIGN IN OR JOIN</a></span>
+      <span><a href="">DETAILS</a></span>
+    </div>
+    <div>
+      <a href="signin.html">Sign In</a>
+      <select>
+        <option>Your Account</option>
+      </select>
+    </div>
+    <div>
+      <img
+        width="50%"
+        height="50%"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU9cMjuePxR1AXt-FOdcdRQylbLZUyxy4SjLY7x5dBK4jbAO1_LGQ00ZkgC28IFo0mYvs&usqp=CAU"
+      />
+      
+    </div>
+  </div>
+  <div id="middle">
+      <div>
+        <a href="home.html"><img
+        width="100%"
+        height="80%"
+        src="https://brandwiki.ru/up/brands/product/190207_4635.jpg"
+      /></a>
+      </div>
+      <div>
+        <a href="">NEW</a>
+        <a href="category.html">WOMEN</a>
+        <a href="">MATERNITY</a>
+        <a href="">MEN</a>
+        <a href="">TEEN</a>
+        <a href="">GIRLS</a>
+        <a href="">BOYS</a>
+        <a href="">TODDLER</a>
+        <a href="">BABY</a>
+        <a href="">JEANS</a>
+        <a style="color: rgb(223, 21, 21)" href="">SALE</a>
+        <a href="">FACTORY</a>
+      </div>
+      <div>
+        <a href="">FIND A STORE</a>
+        <a href="">GAP GOOD REWARDS</a>
+        <a href="">GIFT CARD</a>
+        <input type="text" placeholder="Search:" />
+      </div>
+    </div>`
+}
+
+export default navbar
